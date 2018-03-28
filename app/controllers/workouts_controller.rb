59f0cb9a3ca1616cafdef7792e081b801b0c3921
Jim <wefactoryou@gmail.com>
2018-03-27 17:14:50 -0700

    loop workouts on home page

--- app/controllers/workouts_controller.rb
+++ app/controllers/workouts_controller.rb
@@ -1,5 +1,8 @@
 class WorkoutsController < ApplicationController
+  before_action :find_workout, only: [:show, :edit, :update, :destroy]
+  
   def index
+    @workouts = Workout.all.order("created_at DESC")
   end
   
   def show
@@ -34,5 +37,6 @@ class WorkoutsController < ApplicationController
   end
   
   def find_workout
+    @workout = Workout.find(params[:id])
   end
 end
